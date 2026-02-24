7f50819d257663de72333ef16d3761c1dbe4309c
soniya-builds <soniyasmiles05@gmail.com>
2026-02-24 23:48:01 +0600

    Diagonal Line (m = -1)

---
+++ Second Lab/DiagonalLineMinusSlope.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <iostream>
+using namespace std;
+
+void display(){
+    glClearColor(1.0,1.0,1.0,1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POINTS);
+
+    float xs=0.6, ys=0.1;
+    float xe=0.1, ye=0.9;
+
+    if(xs>xe){
+        swap(xs,xe);
+        swap(ys,ye);
+    }
+
+    float x=xs, y=ys;
+
+    while(x<=xe){
+        glColor3f(1.0, 0.4, 0.7);
+        glVertex2f(x,y);
+        x+=0.0001;
+        y-=0.0001;
+    }
+
+    glEnd();
+    glFlush();
+}
+
+int main(int argc,char* argv[]){
+    glutInit(&argc,argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutCreateWindow("Diagonal Line m=-1");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
